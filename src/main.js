942ddda57c73c87c1d8d9095f83cf26fd74e09fa
ascii <asciislvt@proton.me>
2025-08-23 21:26:49 -0500

    buttons work

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
-import './style.css'
+import "./style.css";
 
-const motd = [
-  "hello, guy!",
-  "good day, guy!",
-  "fancy a drink, guy?",
-  "stuff and things, guy!",
-]
+const MOTD = [
+  "I am you and you are I.",
+  "Touch Grass today!",
+  "Does this rag smell like chloroform to you?",
+  "Be cool, there's someone behind you...",
+];
 
-const motdElement = document.getElementById('motd');
+let motdElement = document.getElementById("motd");
+let buttonElements = document.getElementsByClassName("ContentButton");
+
+for (let i = 0; i < buttonElements.length; i++) {
+  buttonElements[i].addEventListener("click", buttonClicked);
+}
 
 if (motdElement) {
-  let messageIndex = 0;
-  messageIndex = Math.floor(Math.random() * motd.length);
-  motdElement.innerHTML = motd[messageIndex];
+  motdElement.innerHTML = getNewMotd();
+}
+
+function buttonClicked(event) {
+  console.log("A button was clicked!\n" + " Button Clicked:", event.target.id);
+}
+
+function getNewMotd() {
+  let messageIndex;
+
+  messageIndex = Math.floor(Math.random() * MOTD.length);
+  return MOTD[messageIndex];
 }
